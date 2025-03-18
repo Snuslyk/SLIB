08b8680d625d9b707af5752c5eb726fbce690a0a
Роман <127945008+Snuslyk@users.noreply.github.com>
2025-03-19 00:50:29 +0300

    Z>Mkx; ;k x

---
+++ src/main/java/com/github/Snuslyk/slib/generators/ClassGenerators.java
@@ -0,0 +1,56 @@
+package com.github.Snuslyk.slib.generators;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class ClassGenerators {
+
+    public static void generateHibernateObject(Class<?> mainClass, String name, HF... hfs) {
+        String packageName = mainClass.getPackage().getName();
+
+        String code = "package {PACKAGE};\n" +
+                "\n" +
+                "import com.github.Snuslyk.slib.RowData;\n" +
+                "\n" +
+                "import javax.persistence.*;\n" +
+                "\n" +
+                "@Entity\n" +
+                "@Table(name = \"{TABLE}\")\n" +
+                "public class {NAME} implements RowData {\n" +
+                "\n" +
+                "    @Id\n" +
+                "    @GeneratedValue(strategy = GenerationType.IDENTITY)\n" +
+                "    private int id;\n" +
+                "\n" +
+                "    {FIELDS}" +
+                "\n" +
+                "    @Override\n" +
+                "    public int getID() {\n" +
+                "        return id;\n" +
+                "    }\n" +
+                "\n" +
+                "    @Override\n" +
+                "    public int getColorData() {\n" +
+                "        return -1;\n" +
+                "    }\n" +
+                "}\n";
+
+        code = code.replace("{PACKAGE}", packageName);
+        code = code.replace("{NAME}", name);
+
+        StringBuilder fields = new StringBuilder();
+        for (HF hf : hfs) {
+            fields.append(hf.clazz().getSimpleName()).append(" ").append(hf.name()).append("\n");
+        }
+        code = code.replace("{FIELDS}", fields.toString());
+        String tableName = name.substring(0, 1).toLowerCase() + name.substring(1);
+        code = code.replace("{TABLE}", tableName);
+
+        try {
+            Files.writeString(new File(name+".class").toPath(), code);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/github/Snuslyk/slib/generators/HF.java
@@ -0,0 +1,4 @@
+package com.github.Snuslyk.slib.generators;
+
+public record HF(Class<?> clazz, String name) {
+}
